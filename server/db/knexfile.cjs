40c237dd7ef9766b4ed929552ccaefe088a5d407
James Hart <25505178+HartJN@users.noreply.github.com>
2023-02-14 21:19:23 +1300

    test deploy

---
+++ server/db/knexfile.cjs
@@ -0,0 +1,64 @@
+import dotenv from 'dotenv'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import pg from 'pg'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: path.join(__dirname, '../..', '.env') })
+
+const defaults = {
+  useNullAsDefault: true,
+  migrations: {
+    directory: path.resolve(__dirname, 'migrations'),
+    loadExtensions: ['.mjs'],
+  },
+  seeds: {
+    directory: path.resolve(__dirname, 'seeds'),
+    loadExtensions: ['.mjs'],
+  },
+}
+pg.defaults.ssl = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  development: {
+    ...defaults,
+    client: process.env.CLIENT,
+    connection: {
+      database: process.env.DATABASE,
+      user: process.env.PG_USER,
+      password: process.env.PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+  test: {
+    ...defaults,
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:',
+    },
+    useNullAsDefault: true,
+  },
+
+  production: {
+    ...defaults,
+    client: process.env.CLIENT,
+    connection: {
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      database: process.env.PG_DATABASE,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+}
