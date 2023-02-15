bbf24354746d3c19b3a648a8f19759ce6e556d4d
James Hart <25505178+HartJN@users.noreply.github.com>
2023-02-16 12:46:23 +1300

    add layout component

--- client/App.jsx
+++ client/App.jsx
@@ -4,8 +4,6 @@ import './styles/_theme.scss'
 import { Route, Routes } from 'react-router-dom'
 
 import styles from './App.module.scss'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
 import Layout from './Layout'
 import CreateEvent from './pages/CreateEvent/CreateEvent'
 import Dashboard from './pages/Dashboard/Dashboard'
